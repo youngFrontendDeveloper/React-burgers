2f74c197c32c65fe763af3c5f3ef294d75d7ca6c
Nina <frontend-developer@inbox.ru>
2021-12-28 16:39:55 +0300

    Добавила в git папку build

---
+++ build/precache-manifest.578e092128e193a0c7136826e5c51b00.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "215b67032b14db2866487a7a337c87fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6d5ee583cf9191fe125b",
+    "url": "/static/css/main.12e3e781.chunk.css"
+  },
+  {
+    "revision": "1afdd579672622c5c6e2",
+    "url": "/static/js/2.66415a11.chunk.js"
+  },
+  {
+    "revision": "6d5ee583cf9191fe125b",
+    "url": "/static/js/main.ab83b062.chunk.js"
+  },
+  {
+    "revision": "fa27f71bd95d5ca81881",
+    "url": "/static/js/runtime-main.cf8cab74.js"
+  },
+  {
+    "revision": "385ae8f1201379005ad5f888db39e059",
+    "url": "/static/media/burger-logo-4.385ae8f1.jpg"
+  },
+  {
+    "revision": "1d4aace8ece50372c4a71e8334f161a1",
+    "url": "/static/media/down.1d4aace8.svg"
+  },
+  {
+    "revision": "551b5c6b9714b6eafb5739b138485841",
+    "url": "/static/media/up.551b5c6b.svg"
+  }
+]);
